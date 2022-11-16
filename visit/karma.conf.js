bc1c3c159deec2ac9fb90291b604de0c352db12d
Gregory Guerra <gguerratineo@gmail.com>
2022-11-16 17:02:03 -0300

    firts_c

--- visit/karma.conf.js
+++ visit/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/register_visit'),
+      dir: require('path').join(__dirname, './coverage/visit'),
       subdir: '.',
       reporters: [
         { type: 'html' },
